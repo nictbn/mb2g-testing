e0be15697dc9bef30490438acb5fb4232143918b
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-09 22:47:56 +0300

    Integration Test Example

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+    @Test
+    void myFakeIntegrationTest() {
+        System.out.println("My Integration Test Ran");
+    }
+}
