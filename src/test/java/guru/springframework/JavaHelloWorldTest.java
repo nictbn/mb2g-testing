9a8eb4421bd10975a85af7842daf6ccd20623f49
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-10 19:04:40 +0300

    Adds Maven Profiles Example

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        System.out.println("#####################");
+        System.out.println(System.getenv("TEST_HOST"));
+    }
+}
